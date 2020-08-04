ab416f524dadca6e33cd8750c2a5485fdf3cb913
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-04 11:29:47 +0545

    added axios api

---
+++ client/src/components/category-card/card.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Container, Category, LinkMenu } from "./card.styles";
+import { GiFruitBowl, GiManualJuicer, GiCoffeeCup } from "react-icons/gi";
+import { MdRestaurantMenu } from "react-icons/md";
+import { RiCake3Line } from "react-icons/ri";
+import { IoMdBeer } from "react-icons/io";
+export default class Card extends Component {
+  renderList() {
+    if (this.props.Title === "Appetizers") {
+      return <GiFruitBowl size="12rem" />;
+    }
+    if (this.props.Title === "Breakfast") {
+      return <GiCoffeeCup size="12rem" />;
+    }
+    if (this.props.Title === "Main-Menu") {
+      return <MdRestaurantMenu size="12rem" />;
+    }
+    if (this.props.Title === "Dessert") {
+      return <RiCake3Line size="12rem" />;
+    }
+    if (this.props.Title === "Special") {
+      return <GiManualJuicer size="12rem" />;
+    }
+    if (this.props.Title === "Beverage") {
+      return <IoMdBeer size="12rem" />;
+    }
+    else {
+      return <MdRestaurantMenu size="12rem" />;
+    }
+  }
+  render() {
+    return (
+      <Container>
+        <Category>
+          {this.renderList()}
+          <LinkMenu to={this.props.Title}>{this.props.Title}</LinkMenu >
+        </Category>
+      </Container>
+    );
+  }
+}
---
+++ client/src/components/start-screen/start.styles.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const Slogan = styled.div`
+  text-align: center;
+  margin-bottom: 1rem;
+  width: 35rem;
+  display: block;
+  font-size: 4rem;
+  line-height: 1.4;
+  background: -webkit-linear-gradient(#fd6a00, #bc2b55);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+export const Details = styled.div`
+  font-size: 1.6rem;
+  color: #808080;
+`;
