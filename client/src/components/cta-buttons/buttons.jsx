f10b998f7281d7554367454a4e22138a6484ac07
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-04 11:31:25 +0545

    Revert "added axios api"
    This reverts commit ab416f524dadca6e33cd8750c2a5485fdf3cb913.

--- client/src/components/cta-buttons/buttons.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ButtonContainer } from "./buttons.styles";
-const CtaButton = ({ children, ...props }) => (
-  <ButtonContainer {...props}>{children}</ButtonContainer>
-);
-
-export default CtaButton;
