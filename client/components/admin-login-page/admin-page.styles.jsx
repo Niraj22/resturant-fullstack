922392ff70bf153aad28dac346f9624cc4b51052
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-04 11:36:05 +0545

    admin page component

---
+++ client/components/admin-login-page/admin-page.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+`;
+export const BoxContainer = styled.div`
+  z-index: 100000;
+  margin-top: 10rem;
+  width: 40rem;
+  background-color: white;
+  box-shadow: hsla(0, 0%, 0%, 0.15) 0px 4px 12px 0px;
+  padding: 5rem;
+`;
