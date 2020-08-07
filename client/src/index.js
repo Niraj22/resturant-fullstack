8b0925fd587da294c50e0751234c8ccbac1c80f6
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-07 21:14:37 +0545

    restructure indexjs

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import reduxThunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
-import reducers from "./reducers";
 import "./index.css";
 import App from "./App";
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
