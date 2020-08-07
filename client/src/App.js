d757eef19905afc61d53dd73ef2d04133ae0113b
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-07 21:14:23 +0545

    restructure appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
 import "./App.css";
 import Start from "./components/start-screen/start";
 import Menu from "./components/menu/menu";
 import Categories from "./components/categories/categories";
 import Admin from "./components/admin-login-page/admin-page";
-export default function App() {
-  return (
-    <div className="App">
-      <Route exact path="/" component={Start} />
-      <Route exact path="/adminpanel" component={Admin} />
-      <Route exact path="/categories" component={Categories} />
-      <Route exact path="/menu" component={Menu} />
-    </div>
-  );
+import { loadUser } from './actions/authActions'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser())
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Route exact path="/" component={Start} />
+          <Route exact path="/adminpanel" component={Admin} />
+          <Route exact path="/categories" component={Categories} />
+          <Route exact path="/menu" component={Menu} />
+        </div>
+      </Provider>
+    )
+  }
 }
+export default App
