d0fb6edef8421760bab491c937ee868d65f84d3f
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 14:15:04 +0545

    admin users page

---
+++ client/src/components/dashboard-components/admin-users/adminUsers.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminUsers extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Admin Users</h1>
+            </div>
+        )
+    }
+}
