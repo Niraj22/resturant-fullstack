6a205458e8db6a38a2fda0a39fe036282a5195ae
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 21:43:05 +0545

    added styles

---
+++ client/src/components/dashboard-components/admin-categories/adminCat.styles.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+export const ContainerAll = styled.div`
+margin:3rem;
+text-align:center;
+`
+export const HeadContainer = styled.div`
+width:92vw;
+display:flex;
+justify-content:space-between;
+align-content:center;
+`
+export const TextContainer = styled.div`
+font-size:1.7rem;
+line-height: 1.7;
+background: -webkit-linear-gradient(#fd6a00, #bc2b55);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
+export const ButtonAdd = styled.button`
+  font-family: inherit;
+  text-decoration: none;
+  text-align: center;
+  width: 12rem;
+  color: #ffffff;
+  font-weight: lighter;
+  background: linear-gradient(10deg, #fd6000, #bc2b55);
+  cursor: pointer;
+  font-size: 1.4rem;
+  padding: 0,0.5rem;
+  border: 0;
+  transition: all 0.5s;
+  border-radius: 10px;
+  position: relative;
+  border: none;
+  outline: none;
+  &:hover {
+    background: linear-gradient(10deg, #bc2b55, #fd6000);
+    transition: all 0.5s;
+  }
+`
+export const ListContainer = styled.div`
+
+`
+export const List = styled.div`
+background-color:#e6e6e6;
+margin-top:1rem;
+padding:15px;
+display:flex;
+color:#40414d;
+border-bottom:1px solid #40414d;
+justify-content:space-between;
+align-content:center;
+font-size:1.5rem;
+`
+export const Category = styled.div`
+font-family:inherit;
+`
+export const Icon = styled.div`
+
+`
+
