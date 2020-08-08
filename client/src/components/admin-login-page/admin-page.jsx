c6af5ac029131abe5ab6261ab45127cdd3c2677b
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 09:41:33 +0545

    added redirect functionality

--- client/src/components/admin-login-page/admin-page.jsx
+++ client/src/components/admin-login-page/admin-page.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux'
 import AdminLogin from "../admin-login/admin";
+import { withRouter } from 'react-router-dom'
 import { RiAdminLine } from "react-icons/ri";
 import { ReactComponent as Background } from "../../assets/bg.svg";
 import { Container, BoxContainer } from "./admin-page.styles";
-export default function AdminPage() {
-  const styleIcons = { marginBottom: "20px" };
-  return (
-    <Container>
-      <Background
-        style={{
-          height: "45rem",
-          width: "auto",
-          zIndex: "-18239",
-          position: "absolute",
-          opacity: ".4",
-        }}
-      />
-      <BoxContainer>
-        <RiAdminLine size="3rem" style={styleIcons} />
-        <AdminLogin />
-      </BoxContainer>
-    </Container>
-  );
+class AdminPage extends Component {
+  styleIcons = { marginBottom: "20px" };
+  renderAdmin = () => {
+    if (this.props.isAuthenticated === true) {
+      this.props.history.push('/dashboard')
+    }
+    else {
+      return (
+        <Container >
+          <Background
+            style={{
+              height: "45rem",
+              width: "auto",
+              zIndex: "-18239",
+              position: "absolute",
+              opacity: ".4",
+            }}
+          />
+          <BoxContainer>
+            <RiAdminLine size="3rem" style={this.styleIcons} />
+            <AdminLogin />
+          </BoxContainer>
+        </Container >
+      )
+    }
+  }
+  render() {
+    return (
+      <>{this.renderAdmin()}</>
+    )
+  }
 }
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+export default withRouter(connect(mapStateToProps)(AdminPage))
