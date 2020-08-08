e9fed55db2312482b5d7ff9a3fe044cbb2beb422
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 14:14:16 +0545

    updated dashboard

--- client/src/components/admin-dashboard/dashboard.jsx
+++ client/src/components/admin-dashboard/dashboard.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Sidebar from '../dashboard-components/sidebar/sidebar'
+import { Container, Page } from './dashboard.styles'
+// import { Route, Switch } from "react-router-dom";
+// import AdminDashboard from '../dashboard-components/dashboard-page/dashboard'
+// import { } from '../dashboard-components'
+// import { } from '../dashboard-components'
+// import { } from '../dashboard-components'
+// import { } from '../dashboard-components'
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to Dashboard</h1>
-            </div>
+            <Container>
+                <Sidebar />
+                <Page>
+                    <h1>Welcome</h1>
+                </Page>
+            </Container>
         )
     }
 }
---
+++ client/src/components/admin-dashboard/dashboard.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+display:flex;
+`
+export const Page = styled.div`
+width:100%;
+height:100vh;
+padding:1rem;
+`
