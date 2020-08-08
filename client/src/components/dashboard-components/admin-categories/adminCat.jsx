1443878bfe0dd4df0d35b2ca6c65bf151940bfa4
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 14:14:40 +0545

    admin category page

---
+++ client/src/components/dashboard-components/admin-categories/adminCat.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminCat extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Admin Category</h1>
+            </div>
+        )
+    }
+}
