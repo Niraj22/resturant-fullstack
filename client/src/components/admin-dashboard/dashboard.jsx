f219b3a2912085526571b2a8e81725cbdbfbf5df
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 23:24:47 +0545

    added routes

--- client/src/components/admin-dashboard/dashboard.jsx
+++ client/src/components/admin-dashboard/dashboard.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 import Sidebar from '../dashboard-components/sidebar/sidebar'
 import { Container, Page } from './dashboard.styles'
-// import { Route, Switch } from "react-router-dom";
-// import AdminDashboard from '../dashboard-components/dashboard-page/dashboard'
-// import { } from '../dashboard-components'
-// import { } from '../dashboard-components'
-// import { } from '../dashboard-components'
-// import { } from '../dashboard-components'
-
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import AdminCat from '../dashboard-components/admin-categories/adminCat'
 export default class Dashboard extends Component {
     render() {
         return (
-            <Container>
-                <Sidebar />
-                <Page>
-                    <h1>Welcome</h1>
-                </Page>
-            </Container>
+            <BrowserRouter>
+                <Container>
+                    <Sidebar />
+                    <Page>
+                        <Switch>
+                            <Route exact path="/dashboard/admincategories" component={AdminCat} />
+                        </Switch>
+                    </Page>
+                </Container>
+            </BrowserRouter>
         )
     }
 }
