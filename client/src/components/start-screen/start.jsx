3a927d88c2508d46b05ecbf9c839d5acd8739050
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 09:42:04 +0545

    updated start page

--- client/src/components/start-screen/start.jsx
+++ client/src/components/start-screen/start.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Slogan, Details } from "./start.styles";
 import { ReactComponent as Welcome } from "../../assets/welcome-login.svg";
 import CtaButton from "../cta-buttons/buttons";
-export default function Start(props) {
-  return (
-    <Container>
-      <Slogan>Welcome to XYZ</Slogan>
-      <Details>"The pleasure of variety on your plate"</Details>
-      <Welcome style={{ height: "37rem", width: "35rem" }} />
-      <CtaButton to="/adminpanel">Continue as admin</CtaButton>
-      <CtaButton to="/categories">Continue as customer</CtaButton>
-    </Container>
-  );
+class Start extends Component {
+  render() {
+    return (
+      <Container>
+        <Slogan>Welcome to XYZ</Slogan>
+        <Details>"The pleasure of variety on your plate"</Details>
+        <Welcome style={{ height: "37rem", width: "35rem" }} />
+        <CtaButton to="/admin">Continue as admin</CtaButton>
+        <CtaButton to="/categories">Continue as customer</CtaButton>
+      </Container >
+    )
+  }
 }
+
+export default Start
