ff3f7922458fba38c73804009ec6c04603de9c28
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 21:42:56 +0545

    added categories display

--- client/src/components/dashboard-components/admin-categories/adminCat.jsx
+++ client/src/components/dashboard-components/admin-categories/adminCat.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
-
-export default class AdminCat extends Component {
+import { connect } from 'react-redux'
+import { GetCategories } from "../../../actions/categoriesAction";
+import { ContainerAll, HeadContainer, ButtonAdd, TextContainer, listContainer, List, Category, Icon } from './adminCat.styles'
+class AdminCat extends Component {
+    componentDidMount() {
+        this.props.GetCategories();
+    }
+    renderList = () => {
+        if (this.props.categories) {
+            return (
+                <ContainerAll>
+                    <HeadContainer>
+                        <TextContainer>Categories in Inventory</TextContainer>
+                        <ButtonAdd>Add category</ButtonAdd>
+                    </HeadContainer>
+                    <listContainer>
+                        {
+                            this.props.categories.map(({ _id, category }) => (
+                                <List key={_id}>
+                                    <Category>{category}</Category>
+                                    <Icon>X</Icon>
+                                </List>
+                            ))}
+                    </listContainer>
+                </ContainerAll>
+            )
+        }
+    }
     render() {
         return (
-            <div>
-                <h1>Admin Category</h1>
-            </div>
+            <>
+                {this.renderList()}
+            </>
         )
     }
 }
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        categories: state.Categories[0],
+        isAuthenticated: state.auth.isAuthenticated
+    }
+}
+export default connect(mapStateToProps, { GetCategories })(AdminCat)
