215c5a92e104a2b085b9c241a3c1ccca925713e2
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-08 14:14:51 +0545

    admin items page

---
+++ client/src/components/dashboard-components/admin-items/adminItems.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminItems extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Admin Items</h1>
+            </div>
+        )
+    }
+}
