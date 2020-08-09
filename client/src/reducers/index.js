577d76f009384030128cbed39d428d34fa8b6df0
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 11:29:47 +0545

    simple fixes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import Categories from "./getCategoriesReducer";
+import Categories from "./CategoriesReducer";
 import errorReducer from './errorReducers'
 import authReducer from './authReducer'
 export default combineReducers({
