5c9c2fd89d179c42b6d6569dda34b0e5baefc054
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 16:32:32 +0545

    updated style

---
+++ client/src/components/dashboard-components/dashboard-page/dashboard.styles.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+export const ContainerAll = styled.div`
+height:100vh;
+width:100%;
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+`
+export const Icon = styled.div`
+
+`
+export const TextContainer = styled.div`
+font-size:2.6rem;
+line-height: 1.7;
+text-align:center;
+background: -webkit-linear-gradient(#fd6a00, #bc2b55);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
+
+
+
+
+
