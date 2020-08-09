e0a63f4600a6034d1f424d73fa20d2a644bae2ef
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 11:30:24 +0545

    added actions

--- client/src/actions/categoriesAction.js
+++ client/src/actions/categoriesAction.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import { GET_CATEGORIES, CATEGORIES_LOADING } from './types'
+import { GET_CATEGORIES, CATEGORIES_LOADING, ADD_CATEGORIES, DELETE_CATEGORIES } from './types'
 import { returnErrors } from './errorActions'
+import { tokenConfig } from './authActions'
 export const GetCategories = () => dispatch => {
     dispatch(setItemsLoading())
     axios.get('/api/categories').then(res => dispatch({
@@ -9,9 +10,23 @@ export const GetCategories = () => dispatch => {
     }
     ))
         .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
-
 }
-
+export const addCategory = (item) => (dispatch, getState) => {
+    axios.post('/api/categories', item, tokenConfig(getState))
+        .then(res => dispatch({
+            type: ADD_CATEGORIES,
+            payload: res.data
+        }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
+}
+export const deleteCategory = (id) => (dispatch, getState) => {
+    axios.delete(`/api/categories/${id}`, tokenConfig(getState))
+        .then(res => dispatch({
+            type: DELETE_CATEGORIES,
+            payload: id
+        }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
+}
 export const setItemsLoading = () => {
     return {
         type: CATEGORIES_LOADING,
