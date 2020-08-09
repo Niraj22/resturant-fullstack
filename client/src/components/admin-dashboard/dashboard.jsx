68d0a319aad8e4f72791870702a500503bd395d0
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 17:36:56 +0545

    configured dashboard

--- client/src/components/admin-dashboard/dashboard.jsx
+++ client/src/components/admin-dashboard/dashboard.jsx
@@ -4,6 +4,7 @@ import { Container, Page } from './dashboard.styles'
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import AdminCat from '../dashboard-components/admin-categories/adminCat'
 import AdminDashboard from '../dashboard-components/dashboard-page/dashboard'
+import AdminUsers from '../dashboard-components/admin-users/adminUsers'
 export default class Dashboard extends Component {
     render() {
         return (
@@ -14,6 +15,7 @@ export default class Dashboard extends Component {
                         <Switch>
                             <Route exact path="/admincategories" component={AdminCat} />
                             <Route exact path="/admindashboard" component={AdminDashboard} />
+                            <Route exact path="/adminusers" component={AdminUsers} />
                         </Switch>
                     </Page>
                 </Container>
