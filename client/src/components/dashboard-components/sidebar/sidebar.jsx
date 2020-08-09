8d2ce00f169b5ca3294aabfe4169256280d994d5
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 13:17:11 +0545

    added logout

--- client/src/components/dashboard-components/sidebar/sidebar.jsx
+++ client/src/components/dashboard-components/sidebar/sidebar.jsx
@@ -1,68 +1,73 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { Container, Bar, Ulist, Lists, Items, Title } from './sidebar.styles'
+import { connect } from 'react-redux'
+import { logout } from '../../../actions/authActions'
 import { FaHome, FaBriefcase, FaConciergeBell, FaUserCircle, FaFolder, FaUserAlt, FaHistory } from "react-icons/fa";
-class Sidebar extends Component {
-    format = { marginRight: "1rem" }
-    render() {
-        return (
-            <Container>
-                <Bar>
-                    <Ulist>
-                        <Lists>
-                            <Items to='/admindashboard'>
-                                <div>
-                                    <FaHome size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Dashboard
+const Sidebar = (props) => {
+    const format = { marginRight: "1rem" }
+    return (
+        <Container>
+            <Bar>
+                <Ulist>
+                    <Lists>
+                        <Items to='/admindashboard'>
+                            <div>
+                                <FaHome size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Dashboard
                                 </Title>
-                            </Items>
-                            <Items to='/dashboard/admincategories'>
-                                <div>
-                                    <FaFolder size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Categories
+                        </Items>
+                        <Items to='/dashboard/admincategories'>
+                            <div>
+                                <FaFolder size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Categories
                                 </Title>
-                            </Items>
-                            <Items to='/adminitems' >
-                                <div>
-                                    <FaConciergeBell size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Items
+                        </Items>
+                        <Items to='/adminitems' >
+                            <div>
+                                <FaConciergeBell size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Items
                                 </Title>
-                            </Items>
-                            <Items to='/adminusers'>
-                                <div>
-                                    <FaUserAlt size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Users
+                        </Items>
+                        <Items to='/adminusers'>
+                            <div>
+                                <FaUserAlt size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Users
                                 </Title>
-                            </Items>
-                            <Items to='/adminorders'>
-                                <div>
-                                    <FaBriefcase size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Placed orders
+                        </Items>
+                        <Items to='/adminorders'>
+                            <div>
+                                <FaBriefcase size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Placed orders
                                 </Title>
-                            </Items>
-                            <Items to='/logout'>
-                                <div>
-                                    <FaHistory size="2.5rem" style={this.format} color="#40414d" />
-                                </div>
-                                <Title>
-                                    Logout
+                        </Items>
+                        <Items onClick={() => {
+                            props.history.push('/')
+                            props.logout()
+                        }}>
+                            <div>
+                                <FaHistory size="2.5rem" style={format} color="#40414d" />
+                            </div>
+                            <Title>
+                                Logout
                                 </Title>
-                            </Items>
-                        </Lists>
-                    </Ulist>
-                </Bar>
-            </Container>
-        )
-    }
+                        </Items>
+                    </Lists>
+                </Ulist>
+            </Bar>
+        </Container>
+    )
 }
 
-export default Sidebar
+
+export default withRouter(connect(null, { logout })(Sidebar))
