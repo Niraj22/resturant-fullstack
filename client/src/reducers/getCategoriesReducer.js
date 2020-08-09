64904a69c0ab63443bc85e2c15ce598041089f5f
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 11:31:10 +0545

    name changes

--- client/src/reducers/getCategoriesReducer.js
+++
@@ -1,9 +0,0 @@
-import { GET_CATEGORIES } from "../actions/types";
-export default (state = [], action) => {
-  switch (action.type) {
-    case GET_CATEGORIES:
-      return [action.payload];
-    default:
-      return state;
-  }
-};
