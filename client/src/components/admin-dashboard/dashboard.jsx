8bbbb4fb20604c3469cb995cd68e3263f109e9d0
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-09 16:32:21 +0545

    added dashboard

--- client/src/components/admin-dashboard/dashboard.jsx
+++ client/src/components/admin-dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../dashboard-components/sidebar/sidebar'
 import { Container, Page } from './dashboard.styles'
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import AdminCat from '../dashboard-components/admin-categories/adminCat'
+import AdminDashboard from '../dashboard-components/dashboard-page/dashboard'
 export default class Dashboard extends Component {
     render() {
         return (
@@ -11,7 +12,8 @@ export default class Dashboard extends Component {
                     <Sidebar />
                     <Page>
                         <Switch>
-                            <Route exact path="/dashboard/admincategories" component={AdminCat} />
+                            <Route exact path="/admincategories" component={AdminCat} />
+                            <Route exact path="/admindashboard" component={AdminDashboard} />
                         </Switch>
                     </Page>
                 </Container>
--- client/src/components/dashboard-components/dashboard-page/dashboard.jsx
+++ client/src/components/dashboard-components/dashboard-page/dashboard.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-function AdminDashboard() {
-    return (
-        <div>
-            <h1>Welcome to dashboard</h1>
-        </div>
-    )
+import { FaUserCircle } from "react-icons/fa";
+import { loadUser } from '../../../actions/authActions'
+import { ReactComponent as Background } from "../../../assets/bg.svg";
+import { ContainerAll, Icon, TextContainer } from './dashboard.styles'
+class AdminDashboard extends Component {
+    format = { marginBottom: "3rem" }
+    componentDidMount() {
+        this.props.loadUser();
+    }
+    renderList = () => {
+        if (this.props.auth.user) {
+            const user = this.props.auth.user.name
+            return (
+                <ContainerAll>
+                    <Background
+                        style={{
+                            height: "20rem",
+                            width: "auto",
+                            zIndex: "-18239",
+                            position: "absolute",
+                            opacity: ".75",
+                        }}
+                    />
+                    <Icon><FaUserCircle color="#40414d" size="15rem" style={this.format} /></Icon>
+                    <TextContainer>{`Welcome ${user} To Your Dashboard`}</TextContainer>
+                </ContainerAll>
+            )
+        }
+        else {
+            return (<TextContainer>Error loading user</TextContainer>)
+        }
+    }
+    render() {
+        return (
+            <>
+                {this.renderList()}
+            </>
+        )
+    }
 }
-
-export default connect()(AdminDashboard)
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    }
+}
+export default connect(mapStateToProps, { loadUser })(AdminDashboard)
