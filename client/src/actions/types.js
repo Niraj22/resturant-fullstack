8ddf1a8aa4ad923213bbf5f3558843fdd9787849
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-10 12:31:25 +0545

    updated types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,5 +17,5 @@ export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS"
 export const USERS_LOADING = "USERS_LOADING"
 export const USERS_LOADED = "USERS_LOADED"
 export const GET_USERS = "GET_USERS";
-export const ADD_USERS = "ADD_USERS"
+export const ADD_USER = "ADD_USER"
 export const DELETE_USER = "DELETE_USER"
