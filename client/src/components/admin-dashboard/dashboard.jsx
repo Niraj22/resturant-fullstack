d27f416a91b3b98f1a464f50768581a5559b6321
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-10 21:53:28 +0545

    added changes

--- client/src/components/admin-dashboard/dashboard.jsx
+++ client/src/components/admin-dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Sidebar from '../dashboard-components/sidebar/sidebar'
 import { Container, Page } from './dashboard.styles'
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom"
 import AdminCat from '../dashboard-components/admin-categories/adminCat'
 import AdminDashboard from '../dashboard-components/dashboard-page/dashboard'
 import AdminUsers from '../dashboard-components/admin-users/adminUsers'
@@ -12,11 +12,9 @@ export default class Dashboard extends Component {
                 <Container>
                     <Sidebar />
                     <Page>
-                        <Switch>
-                            <Route exact path="/admincategories" component={AdminCat} />
-                            <Route exact path="/admindashboard" component={AdminDashboard} />
-                            <Route exact path="/adminusers" component={AdminUsers} />
-                        </Switch>
+                        <Route exact path="/dashboard/admincategories" component={AdminCat} />
+                        <Route exact path="/dashboard/admindashboard" component={AdminDashboard} />
+                        <Route exact path="/dashboard/adminusers" component={AdminUsers} />
                     </Page>
                 </Container>
             </BrowserRouter>
