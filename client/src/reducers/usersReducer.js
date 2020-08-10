417c8af53cd585185a6c255b2648cf116664cd4c
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-10 13:29:18 +0545

    made user reducer work

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
 
         case ADD_USER:
             return {
-                ...state
+                users: [...state.users, action.payload], loading: false
             }
         case DELETE_USER:
             return {
