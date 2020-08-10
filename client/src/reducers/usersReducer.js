e70af9c71fe71ed2d8a391a73e1531fbb8bd5e40
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-10 12:31:50 +0545

    added reducers

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,4 +1,4 @@
-import { USERS_LOADED } from "../actions/types";
+import { USERS_LOADED, ADD_USER, DELETE_USER } from "../actions/types";
 const initialState = {
     loading: false
 }
@@ -6,6 +6,16 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case USERS_LOADED:
             return { ...state, users: action.payload, loading: false }
+
+        case ADD_USER:
+            return {
+                ...state
+            }
+        case DELETE_USER:
+            return {
+                ...state,
+                users: state.users.filter(user => user._id !== action.payload)
+            }
         default:
             return state;
     }
