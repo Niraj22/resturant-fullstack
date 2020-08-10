bf4796ccedfd74d37882ad32e20e93aad0379668
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-10 12:31:39 +0545

    added actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GET_USERS, USERS_LOADING, USERS_LOADED, ADD_USERS, DELETE_USER } from './types'
+import { USERS_LOADING, USERS_LOADED, ADD_USER, DELETE_USER } from './types'
 import { returnErrors } from './errorActions'
 import { tokenConfig } from './authActions'
 export const GetUsers = () => (dispatch, getState) => {
@@ -12,6 +12,23 @@ export const GetUsers = () => (dispatch, getState) => {
         )
     })
         .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
+}
+
+export const addUser = (item) => (dispatch, getState) => {
+    axios.post('/api/users', item, tokenConfig(getState))
+        .then(res => dispatch({
+            type: ADD_USER
+        }))
+        .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
+}
+
+export const deleteUser = (id) => (dispatch, getState) => {
+    axios.delete(`/api/users/${id}`, tokenConfig(getState))
+        .then(res => dispatch({
+            type: DELETE_USER,
+            payload: id
+        }))
+        .catch(err => { console.log(err) })
 }
 
 export const setItemsLoading = () => {
