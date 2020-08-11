9655610aae990c91be3043784767a13e6f5be0af
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-11 10:29:11 +0545

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,13 @@ import Start from "./components/start-screen/start";
 import Dashboard from './components/admin-dashboard/dashboard'
 import AdminPage from './components/admin-login-page/admin-page'
 import Categories from "./components/categories/categories";
+//!admin routes
+import Wrapper from './components/wrapper/wrapper.jsx'
+import AdminDashboard from "./components/dashboard-components/dashboard-page/dashboard"
 import AdminCat from './components/dashboard-components/admin-categories/adminCat'
+import AdminUsers from './components/dashboard-components/admin-users/adminUsers'
+import AdminItems from './components/dashboard-components/admin-items/adminItems'
+import Orders from './components/dashboard-components/placed-orders/orders'
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -29,9 +35,11 @@ class App extends Component {
             <Route exact path="/" component={Start} />
             <Route exact path="/admin" component={AdminPage} />
             <Route exact path="/categories" component={Categories} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/admincat" component={AdminCat} />
+            <Route exact path="/admindashboard" component={Wrapper(AdminDashboard)} />
+            <Route exact path="/admincategories" component={Wrapper(AdminCat)} />
+            <Route exact path="/adminusers" component={Wrapper(AdminUsers)} />
+            <Route exact path="/adminitems" component={Wrapper(AdminItems)} />
+            <Route exact path="/adminorders" component={Wrapper(Orders)} />
           </div>
         </Provider>
       </BrowserRouter>
