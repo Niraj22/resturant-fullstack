a23de2defad918cb2e45f244ac0ff8b3c772cf4f
RacL <rashil22ulak@gmail.com>
2020-08-18 21:11:43 +0545

    image upload

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const app = express()
-const helmet = require('helmet')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const helmet = require("helmet");
+
 //body-parser
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(fileupload());
+
 //use helmet
-app.use(helmet())
+app.use(helmet());
 //DB config
-mongoose.connect('mongodb://localhost/resturant',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
-    .then(() => console.log("connected to database"))
-    .catch((error) => console.log("not connected to db : ", error))
+mongoose
+  .connect("mongodb://localhost/resturant", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((error) => console.log("not connected to db : ", error));
 
 //use routes
-app.use('/api/categories', require('./routes/api/categories'))
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/items', require('./routes/api/item'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use("/api/categories", require("./routes/api/categories"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/items", require("./routes/api/item"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 //Serve our static assets
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-const port = process.env.PORT || 5001
-app.listen(port, () => { console.log(`server started on port ${port}`) }) 
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
