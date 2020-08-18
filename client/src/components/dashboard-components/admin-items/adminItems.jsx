d827593dfedfa77e94d5720b2df6ab8ef15d1a82
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-18 17:37:51 +0545

    added admin items

--- client/src/components/dashboard-components/admin-items/adminItems.jsx
+++ client/src/components/dashboard-components/admin-items/adminItems.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import { ContainerAll, HeadContainer, TextContainer } from '../common/common'
 export default class AdminItems extends Component {
     render() {
         return (
-            <div>
-                <h1>Admin Items</h1>
-            </div>
+            <ContainerAll>
+                <HeadContainer>
+                    <TextContainer>Admin Items</TextContainer>
+                </HeadContainer>
+            </ContainerAll>
         )
     }
 }
