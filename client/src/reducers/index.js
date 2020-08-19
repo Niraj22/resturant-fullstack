df650bd5eaaa7d6e79f01e51704983e41126bad6
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-19 12:15:25 +0545

    added item reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import Categories from "./CategoriesReducer";
 import errorReducer from './errorReducers'
 import authReducer from './authReducer'
 import usersReducer from './usersReducer'
+import itemsReducer from './itemsReducer'
 export default combineReducers({
   Categories: Categories,
   error: errorReducer,
   auth: authReducer,
-  users: usersReducer
+  users: usersReducer,
+  items: itemsReducer
 });
