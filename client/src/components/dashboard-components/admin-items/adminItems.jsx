be0fd671735184ecdcbf37c8c468c2ab2b67c4ba
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-21 18:07:57 +0545

    updated admin items

--- client/src/components/dashboard-components/admin-items/adminItems.jsx
+++ client/src/components/dashboard-components/admin-items/adminItems.jsx
@@ -1,15 +1,55 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { ContainerAll, HeadContainer, TextContainer } from '../common/common-header'
+import { GetItems } from '../../../actions/itemActions'
+import { connect } from 'react-redux'
+import { ItemsContainer } from './adminItems.styles'
+import CtaButton from '../../cta-buttons/buttons'
 import ItemModal from '../item-modal/item'
-export default class AdminItems extends Component {
+import ItemCard from '../../item-card/ItemCard'
+class AdminItems extends Component {
+    componentDidMount() {
+        this.props.GetItems()
+    }
+    renderList = () => {
+        if (this.props.isAuthenticated !== true) {
+            this.props.history.push('/admin')
+            return
+        }
+        if (this.props.items.items) {
+            const data = this.props.items.items
+            return (
+                <ContainerAll>
+                    <HeadContainer>
+                        <TextContainer>Admin Items</TextContainer>
+                        <ItemModal />
+                    </HeadContainer>
+                    <ItemsContainer>
+                        {
+                            data.map(el => (
+                                <ItemCard key={el._id} data={el} />
+                            ))
+                        }
+                    </ItemsContainer>
+                    <div style={{ marginTop: "2rem" }}>
+                        <CtaButton to="/">Load More</CtaButton>
+                    </div>
+                </ContainerAll>
+            )
+        }
+    }
     render() {
         return (
-            <ContainerAll>
-                <HeadContainer>
-                    <TextContainer>Admin Items</TextContainer>
-                    <ItemModal />
-                </HeadContainer>
-            </ContainerAll>
+            <>
+                {this.renderList()}
+            </>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        items: state.items,
+        isAuthenticated: state.auth.isAuthenticated
+    }
+}
+export default withRouter(connect(mapStateToProps, { GetItems })(AdminItems))
