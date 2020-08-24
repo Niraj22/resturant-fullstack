3c18b319850e274102dccec3bfcd6adf91abdeda
Niraj budhathoki <nirajbudhathoki3@gmail.com>
2020-08-24 21:29:16 +0545

    added items actions

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { GET_ITEMS, ADD_ITEM, ITEMS_LOADING } from './types'
 import { returnErrors } from './errorActions'
 import { tokenConfig } from './authActions'
-export const GetItems = () => dispatch => {
+export const GetItems = (page = 1) => dispatch => {
     dispatch(setItemsLoading())
-    axios.get('/api/items').then(res =>
+    axios.get(`/api/items/?page=${page}&limit=6`).then(res =>
         dispatch({
             type: GET_ITEMS,
-            payload: res.data.data
+            payload: res.data
         }
         )
     )
@@ -21,7 +21,6 @@ export const PostItem = (item) => (dispatch, getState) => {
         }))
         .catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
 }
-
 
 export const setItemsLoading = () => {
     return {
