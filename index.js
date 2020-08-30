8aebe83ae0d45033e2cbfc1005b82a323a6c1604
RacL <rashil22ulak@gmail.com>
2020-08-30 14:34:57 +0545

    db connection fixed

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(helmet());
 app.use("/", express.static("build"));
+app.use(helmet());
 
 // app.use(fileupload());
 
