7de0f7387fceaa33569dbb52e13f0e5285526da8
RacL <rashil22ulak@gmail.com>
2020-08-30 14:11:44 +0545

    api endpont call fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    "/api/**",
-    createProxyMiddleware({
-      target: "http://restaurant-beta.herokuapp.com",
-      changeOrigin: true,
-    })
-  );
-};
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     "/api/**",
+//     createProxyMiddleware({
+//       target: "http://restaurant-beta.herokuapp.com",
+//       changeOrigin: true,
+//     })
+//   );
+// };
