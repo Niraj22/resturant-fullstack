30bb5ba453faaddfb0a666c83e334e3bb1fd6944
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2020-08-31 11:35:01 +0545

    proxy setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
 // const { createProxyMiddleware } = require("http-proxy-middleware");
 // module.exports = function (app) {
-//   app.use(
-//     "/api/**",
-//     createProxyMiddleware({
-//       target: "http://restaurant-beta.herokuapp.com",
-//       changeOrigin: true,
-//     })
-//   );
+//     app.use(
+//         "/api/**",
+//         createProxyMiddleware({
+//             target: "http://localhost:5001/",
+//             changeOrigin: true,
+//         })
+//     );
 // };
