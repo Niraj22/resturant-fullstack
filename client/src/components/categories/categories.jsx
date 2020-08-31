01b830af9b48836054e165016332e365eb4be2dd
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2020-08-31 11:31:00 +0545

    added loader and ui changes

--- client/src/components/categories/categories.jsx
+++ client/src/components/categories/categories.jsx
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { GetCategories } from "../../actions/categoriesAction";
 import Card from "../category-card/card";
+import { LoadingContainer } from '../dashboard-components/common/loadingContainer'
+import Loader from '../loader/loader'
 import { Slogan, CategoryContainer } from "./categories.styles";
 class Categories extends Component {
   componentDidMount() {
     this.props.GetCategories();
   }
   renderCards() {
+    if (this.props.categories.loading) {
+      return (
+        <LoadingContainer>
+          <Loader />
+        </LoadingContainer>
+      )
+    }
     var data = this.props.categories.items
     if (data) {
       return data.map(el => {
