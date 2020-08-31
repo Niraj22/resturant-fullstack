8abb1af29491f10d6d89ff8c0a5c1179e133d14b
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2020-08-31 11:34:48 +0545

    use hash router

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import apiCall from "./apiCall";
-ReactDOM.render(<App />, document.getElementById("root"));
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>, document.getElementById("root"));
